79fc315786b46178fab3adf9f1695c78c70402f6
agperkins <alexandrasaff@gmail.com>
2018-04-11 17:51:01 -0500

    Add const Button

---
+++ src/components/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Button = styled.button`
+color: #555;
+font-size: 16px;
+text-align: center;
+font-weight: 700;
+line-height: 1.2;
+letter-spacing: -.4px;
+background-color: lightblue;
+font-family: -apple-system, sans-serif;
+padding: 15px;
+`
+export default Button
