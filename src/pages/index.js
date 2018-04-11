dc6db150a3f2dd889b9e103aad1ee25ff4ab16f3
agperkins <alexandrasaff@gmail.com>
2018-04-11 17:51:15 -0500

    Add button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Button from '../components/Button'
 
 const IndexPage = () => (
   <div>
     <h1>Practice Header</h1>
     <p>Some stuff will go here.</p>
     <p>NOT here.</p>
+    <Button>Dis da button</Button>
   </div>
 )
 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Button from '../components/Button'
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <Button>She Thiqq</Button>
   </div>
 )
 
