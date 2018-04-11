9fc3b98631b14a7c9f9fb89200e301009bab5035
agperkins <alexandrasaff@gmail.com>
2018-04-11 17:07:53 -0500

    Rename page-3 Index

---
+++ src/pages/page-3/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import Image from './Ali.jpg'
+
+const ThirdPage = () => (
+  <div>
+    <h1>Alexandra Perkins</h1>
+    <p>Welcome to page 3</p>
+    <img src={Image} alt="profile picture"/>
+  </div>
+)
+
+export default ThirdPage
