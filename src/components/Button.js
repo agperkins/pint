6307b5bbd70440a06e281985eba5499bdc5237f7
agperkins <alexandrasaff@gmail.com>
2018-04-13 11:42:38 -0500

    Style button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 
 
 const Button = styled.button`
-color: #555;
+color: white;
 font-size: 16px;
 text-align: center;
 font-weight: 700;
 line-height: 1.2;
 letter-spacing: -.4px;
-background-color: lightblue;
+background-color: crimson;
 font-family: -apple-system, sans-serif;
 padding: 15px;
 `
