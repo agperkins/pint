faf4fc9b673c8428e1a213115351a92679ea49c4
agperkins <alexandrasaff@gmail.com>
2018-04-13 11:42:57 -0500

    Add accordion

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Button from '../components/Button'
+import ReactDOM from 'react-dom';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+} from 'react-accessible-accordion'
+
+import 'react-accessible-accordion/dist/minimal-example.css';
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Button>She Thiqq</Button>
+    <Accordion>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h2>About</h2>
+            </AccordionItemTitle>
+            <AccordionItemBody>
+                <p>I'm using this site to practice different React features</p>
+            </AccordionItemBody>
+        </AccordionItem>
+        <AccordionItem>
+            <AccordionItemTitle>
+                <h2>Payment</h2>
+                <AccordionItemBody>
+                <p>PayPal, Creditcard</p>
+                </AccordionItemBody>
+            </AccordionItemTitle>
+        </AccordionItem>
+    </Accordion>
   </div>
 )
 
