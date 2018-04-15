96f951d3e1bfd165925ab0e17ad33ea10b6c3d8b
agperkins <alexandrasaff@gmail.com>
2018-04-15 14:42:13 -0500

    Make button show image

--- src/pages/page-3/index.js
+++ src/pages/page-3/index.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Image from './Ali.jpg'
+import Button from '../../components/Button'
 
-const ThirdPage = () => (
-  <div>
-    <h1>Alexandra Perkins</h1>
-    <p>Welcome to page 3</p>
-    <img src={Image} alt="profile picture"/>
-  </div>
-)
+
+
+class ThirdPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clicks: 0,
+      image: false,
+    }
+  }
+
+  handleClick() {
+    this.setState((prevState) => ({
+         clicks: prevState.clicks + 1,
+      }));
+  }
+
+  handleImage() {
+    this.setState((prevState) => ({
+         image: !prevState.image
+      }));
+  }
+ 
+  render () {
+    return (
+      <div>
+      <h1>Alexandra Perkins</h1>
+      <p>The count is {this.state.clicks}</p>
+      <Button onClick={this.handleClick.bind(this)}>CLick to see me</Button>
+      <Button background={this.state.image === false && ('rebeccapurple') } onClick={this.handleImage.bind(this)}>{this.state.image.toString()}</Button>
+      {this.state.image === true && (
+        <img src={Image} alt="profile picture"/>
+      )}
+     
+    </div>
+    )
+  }
+}
 
 export default ThirdPage
+
