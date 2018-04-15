ebc71305ed1538b7ac5f4370e5ef7870ddafeab3
agperkins <alexandrasaff@gmail.com>
2018-04-15 14:41:32 -0500

    Add background color props

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,14 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 
-const Button = styled.button`
+const Button = styled.button.attrs({
+  background: props => props.background || 'black'
+})
+`
 color: white;
 font-size: 16px;
 text-align: center;
 font-weight: 700;
 line-height: 1.2;
 letter-spacing: -.4px;
-background-color: crimson;
+background-color: ${props => props.background};
 font-family: -apple-system, sans-serif;
 padding: 15px;
 `
