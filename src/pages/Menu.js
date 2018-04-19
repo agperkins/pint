04cd1db1a97b5c40249e81a62ac80c8701f64568
agperkins <alexandrasaff@gmail.com>
2018-04-19 18:33:12 -0500

    Add copy

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -2,7 +2,78 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Menu = () => (
-  <h1>Dis da menu</h1>
+<div>
+  <h1>HOOLIGANS</h1>
+    <h2>Wraps</h2>
+      <h3>Wrap $6.00 - Combo $8.50</h3>
+        <p><strong>Turkey Melt</strong> - Thinly sliced turkey breast hot off the grill with melted swiss cheese, mayo, mustard, lettuce, and tomato</p>
+        <p><strong>Ham & Swiss</strong> - Thinly sliced turkey ham hot off the grill with melted swiss cheese, mayo, mustard, lettuce, and tomato</p>
+        <p><strong>Pita Club</strong> - Combo of turkey, turkey ham, and turkey bacon topped with melted American cheese, mayo, mustard, lettuce, and tomato</p>
+        <p><strong>Chicken Salad</strong> - Our freshly chopped chicken mixed with mayo, celery, lettuce, and tomato</p>
+        <p><strong>Falafel</strong> - Vegetarian hushpuppy made of freshly ground chick peas with parsley, onion, garlic and spices. Lightly friend and rolled inside fresh pita bread, dressed with hummus, lettuce, and tomato</p>
+        <p><strong>Vegetarian</strong> - Grilled vegetables served with hummus, lettuce, and tomato</p>
+        <p><strong>Philly Steak</strong> - Grilled thin sliced sirloin with onion and bell pepper topped with swiss cheese and dressed with lettuce</p>
+        <p><strong>Ribeye Steak</strong> - Our famous strip of fresh ribeye with sautéed onion and mushroom off the grill, dressed with mayo, mustard, lettuce, and tomato</p>
+        <p><strong>Kafta Kabob</strong> - A mix of ground beef, parsley garlic, onion, and spices dressed with hummus, lettuce, and tomato</p>
+        <p><strong>Gyro</strong> - A combination of seasoned beef and lamb slow cooked on a rotisserie served in a pita bread and dressed with tzatziki sauce, lettuce, and tomato</p>
+        <p><strong>Chicken Gyro</strong> - Greek style chicken roled in pina bread and dressed with tzatziki sauce, lettuce, and tomato</p>
+        <p><strong>Mediterranean Chicken</strong> - Grilled chunks of chicken breast, onion and pepper dressed with hummus, lettuce, and tomato</p>
+        <p><strong>Chicken Philly</strong> - Tender chicken grilled with onions and bell peppers, topped with swiss cheese and dressed with lettuce, tomato, and mayo</p>
+        <p><strong>Turbo Gyro</strong> - Gyro meat topped iwth lettuce, tomato, jalapenño, and ranch dressing</p>
+        <p><strong>Your Way Gyro</strong> - Create your own gyro, but don't go crazy!</p>
+    <h2>Burgers</h2>
+      <h3>All burgers are dressed with mayo, mustard, lettuce, tomato, and pickle on a bun. Combo includes fries and a drink</h3>
+        <p><strong>Hamburger</strong> - $4.75, Combo $7.25</p>
+        <p><strong>Cheeseburger</strong> - $5.05, Combo $7.55</p>
+        <p><strong>Bacon Cheeseburger</strong> - $5.35, Combo $7.85</p>
+        <p><strong>Double Cheeseburger</strong> - $6.55, Combo $9.05</p>
+        <p><strong>Double Bacon Cheeseburger</strong> - $6.85, Combo $9.35</p>
+    <h2>Entrees</h2>
+      <h3>Plate with salad, rice, hummus or tzatziki dip, and hot pita bread. Kabob skewers served with grilled onion and pepper</h3>
+        <p><strong>Falafel Plate</strong> - $10.99</p>
+        <p><strong>Gyro Plate</strong> - $10.99</p>
+        <p><strong>Kafta Kabob Plate</strong> - $11.99</p>
+        <p><strong>Chicken Kabob Plate</strong> - $10.99</p>
+        <p><strong>Grilled Tilapia Plate</strong> - $11.99</p>
+        <p><strong>Beef Kabob Plate</strong> - $11.99</p>
+        <p><strong>Shrimp Kabob Plate</strong> - $11.99</p>
+        <p><strong>Combination Plate</strong>Choose any two of the above - $15.99</p>
+    <h2>Other Favorites</h2>
+        <p><strong>Chicken Fingers</strong>Served with fries and honey mustard - $7.50</p>
+        <p><strong>Swiss Chicken and Mushroom</strong> - $5.29</p>
+        <p><strong>Hot Wings and Fries</strong> - $7.50</p>
+        <p><strong>BLT</strong> - $3.25</p>
+        <p><strong>Grilled Cheese on Sourdough bread</strong> - $2.65</p>
+        <p><strong>BBQ Beef or Chicken Sandwich</strong> - $4.99</p>
+    <h2>Sampler</h2>
+      <p>Try all of our Mediterranean Favorites for $13.95! Sampler includes falafel, kafta, tzatziki dip, hummus dip, tabuli, wild rice, and hot pita bread</p>
+    <h2>Soups & Salads</h2>
+      <p><strong>Greek Salad</strong>Salad with feta cheese, black olives, pepperoncini, & pita bread - $7.49</p>
+      <p><strong>Chef Salad</strong>A large salad with turkey-ham, American cheese, Swiss cheese, & pita bread - $7.49</p>
+      <p><strong>Grilled Chicken Salad</strong>A large salad topped with shredded cheese and grilled chicken - $7.49</p>
+      <p><strong>Chicken Salad Plate</strong>Our amazing chicken salad served on a bed of lettuce with tomatoes and pickles - $6.49</p>
+      <p><strong>Garden Salad</strong> - $4.59</p>
+      <p><strong>Lentil Soup</strong>Made with lentils, onion, salt, and pepper - $2.99</p>
+    <h2>Specialty Spuds</h2>
+      <p><strong>Simple Potato</strong>Topped with butter and sour cream - $3.49</p>
+      <p><strong>Stuffed Potato</strong>Topped with butter, sour cream, cheddar cheese, green onion, and bacon bits - $6.99</p>
+      <p><strong>Barbecue Chicken Potato</strong>Stuffed potato topped with our famous gyro meat - $6.99</p>
+      <p><strong>Gyro Potato</strong>Stuffed potato topped with our famous gyro meat - $6.99</p>
+      <p><strong>Philly Steak Potato</strong>Stuffed potato topped with grilled thin sliced sirloin with onion, pepper, and Swiss cheese - $6.99</p>
+
+
+
+
+
+
+
+
+
+
+ 
+  </div>
+
+
 )
 
 export default Menu
