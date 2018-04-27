bddea819a107430a1d28cd5d2ef95a7184a89961
agperkins <alexandrasaff@gmail.com>
2018-04-27 11:38:43 -0500

    add images to Grid Practice

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Button from '../components/Button'
+import DaImage from '../img/one.jpg'
+import '../styles/grid.css'
 
+// It is working, wrow.
 const IndexPage = () => (
-  <div>
-    <h1>Practice Header</h1>
-    <p>Some stuff will go here.</p>
-    <p>NOT here.</p>
-    <Button>Dis da button</Button>
-  </div>
+<main>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+<img src={DaImage} alt="photo one"/>
+
+</main>
 )
 
 export default IndexPage
